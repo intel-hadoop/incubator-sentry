76f80546065f1cedc5a902490e9811c0b5230c5e
Huang Xiaomeng <xiaomeng.huang@intel.com>
2014-11-24 20:05:29 +0800

    SENTRY-505

--- sentry-binding/sentry-binding-hive/src/main/java/org/apache/sentry/binding/hive/v2/impl/DefaultSentryAuthorizationValidator.java
+++ sentry-binding/sentry-binding-hive/src/main/java/org/apache/sentry/binding/hive/v2/impl/DefaultSentryAuthorizationValidator.java
@@ -21,13 +21,22 @@ import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.hive.conf.HiveConf;
+import org.apache.hadoop.hive.ql.metadata.AuthorizationException;
+import org.apache.hadoop.hive.ql.plan.HiveOperation;
 import org.apache.hadoop.hive.ql.security.HiveAuthenticationProvider;
 import org.apache.hadoop.hive.ql.security.authorization.plugin.HiveOperationType;
 import org.apache.hadoop.hive.ql.security.authorization.plugin.HivePrivilegeObject;
 import org.apache.sentry.binding.hive.authz.HiveAuthzBinding;
+import org.apache.sentry.binding.hive.authz.HiveAuthzBinding.HiveHook;
+import org.apache.sentry.binding.hive.authz.HiveAuthzPrivileges;
+import org.apache.sentry.binding.hive.authz.HiveAuthzPrivilegesMap;
 import org.apache.sentry.binding.hive.conf.HiveAuthzConf;
 import org.apache.sentry.binding.hive.v2.SentryAuthorizationValidator;
 import org.apache.sentry.binding.hive.v2.util.SentryAccessControlException;
+import org.apache.sentry.binding.hive.v2.util.SentryAuthorizerUtil;
+import org.apache.sentry.core.common.Subject;
+import org.apache.sentry.core.model.db.DBModelAuthorizable;
 
 public class DefaultSentryAuthorizationValidator extends SentryAuthorizationValidator {
 
@@ -38,12 +47,35 @@ public class DefaultSentryAuthorizationValidator extends SentryAuthorizationVali
     super(authzConf, hiveAuthzBinding, authenticator);
   }
 
+  public DefaultSentryAuthorizationValidator(HiveHook hiveHook, HiveConf conf, HiveAuthzConf authzConf,
+      HiveAuthenticationProvider authenticator) throws Exception {
+    this(authzConf, new HiveAuthzBinding(hiveHook, conf, authzConf), authenticator);
+  }
+
+  public DefaultSentryAuthorizationValidator(HiveConf conf, HiveAuthzConf authzConf,
+      HiveAuthenticationProvider authenticator) throws Exception {
+    this(HiveHook.HiveServer2, conf, authzConf, authenticator);
+  }
+
   @Override
   public void checkPrivileges(HiveOperationType hiveOpType,
       List<HivePrivilegeObject> inputHObjs,
-      List<HivePrivilegeObject> outputHObjs)
-      throws SentryAccessControlException {
-    // TODO Auto-generated method stub
+      List<HivePrivilegeObject> outputHObjs) throws SentryAccessControlException {
+    HiveOperation hiveOp = SentryAuthorizerUtil.convert2HiveOperation(hiveOpType);
+    HiveAuthzPrivileges stmtAuthPrivileges = HiveAuthzPrivilegesMap.getHiveAuthzPrivileges(hiveOp);
+    if (stmtAuthPrivileges == null) {
+      throw new SentryAccessControlException("Sentry don't support " + hiveOp);
+    }
 
+    List<List<DBModelAuthorizable>> inputHierarchyList =
+        SentryAuthorizerUtil.convert2SentryPrivilegeList(hiveAuthzBinding.getAuthServer(), inputHObjs);
+    List<List<DBModelAuthorizable>> outputHierarchyList =
+        SentryAuthorizerUtil.convert2SentryPrivilegeList(hiveAuthzBinding.getAuthServer(), outputHObjs);
+    try {
+      hiveAuthzBinding.authorize(hiveOp, stmtAuthPrivileges, new Subject(authenticator.getUserName()),
+          inputHierarchyList, outputHierarchyList);
+    } catch (AuthorizationException e) {
+      throw new SentryAccessControlException(e);
+    }
   }
 }
